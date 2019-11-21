35b884d834e54619abd4d880d365e819c835a5fd
Lucien <562211065@qq.com>
2019-11-21 23:17:09 +0800

    new

--- src/main/java/com/ds/factory/exception/DSException.java
+++ src/main/java/com/ds/factory/exception/DSException.java
@@ -12,11 +12,15 @@ public class DSException {
     public static void readFail(Logger logger, Exception e) {
         logger.error("异常码[{}],异常提示[{}],异常[{}]",
                 ExceptionConstants.DATA_READ_FAIL_CODE, ExceptionConstants.DATA_READ_FAIL_MSG,e);
+        throw new BusinessRunTimeException(ExceptionConstants.DATA_READ_FAIL_CODE,
+                ExceptionConstants.DATA_READ_FAIL_MSG);
     }
 
     public static void writeFail(Logger logger, Exception e) {
         logger.error("异常码[{}],异常提示[{}],异常[{}]",
                 ExceptionConstants.DATA_WRITE_FAIL_CODE,ExceptionConstants.DATA_WRITE_FAIL_MSG,e);
+        throw new BusinessRunTimeException(ExceptionConstants.DATA_WRITE_FAIL_CODE,
+                ExceptionConstants.DATA_WRITE_FAIL_MSG);
     }
 
 
