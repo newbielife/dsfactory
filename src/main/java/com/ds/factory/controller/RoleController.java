0731f0787fedd84640b80fac9e850dcddf56eefa
Lucien <562211065@qq.com>
2019-11-26 09:43:53 +0800

    role查询

--- src/main/java/com/ds/factory/controller/RoleController.java
+++ src/main/java/com/ds/factory/controller/RoleController.java
@@ -2,21 +2,25 @@ package com.ds.factory.controller;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.ds.factory.constants.BusinessConstants;
 import com.ds.factory.constants.ExceptionConstants;
 import com.ds.factory.datasource.entities.Role;
 import com.ds.factory.exception.BusinessRunTimeException;
 import com.ds.factory.service.Service.RoleService;
 import com.ds.factory.service.Service.UserBusinessService;
+import com.ds.factory.utils.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static com.ds.factory.utils.ResponseJsonUtil.returnJson;
 
 /**
  * @author gyc_lyz_ztm
@@ -67,7 +71,7 @@ public class RoleController {
         return arr;
     }
 
-    @PostMapping(value = "/list")
+    @GetMapping(value = "/list")
     public List<Role> list(HttpServletRequest request)throws Exception {
         return roleService.getRole();
     }
@@ -83,6 +87,34 @@ public class RoleController {
                     ExceptionConstants.ROLE_DELETE_FAILED_MSG);
         }
         return result;
+    }
+
+    @GetMapping(value = "/getAllRole")
+    public String getList(@RequestParam(value = Constants.PAGE_SIZE, required = false) Integer pageSize,
+                          @RequestParam(value = Constants.CURRENT_PAGE, required = false) Integer currentPage,
+                          @RequestParam(value = Constants.SEARCH, required = false) String search,
+                          HttpServletRequest request)throws Exception {
+        Map<String, String> parameterMap = ParamUtils.requestToMap(request);
+        parameterMap.put(Constants.SEARCH, search);
+        PageQueryInfo queryInfo = new PageQueryInfo();
+        Map<String, Object> objectMap = new HashMap<String, Object>();
+        if (pageSize != null && pageSize <= 0) {
+            pageSize = 10;
+        }
+        String offset = ParamUtils.getPageOffset(currentPage, pageSize);
+        if (StringUtil.isNotEmpty(offset)) {
+            parameterMap.put(Constants.OFFSET, offset);
+        }
+        List<?> list = roleService.getRole();
+        objectMap.put("page", queryInfo);
+        if (list == null) {
+            queryInfo.setRows(new ArrayList<Object>());
+            queryInfo.setTotal(BusinessConstants.DEFAULT_LIST_NULL_NUMBER);
+            return returnJson(objectMap, "查找不到数据", ErpInfo.OK.code);
+        }
+        queryInfo.setRows(list);
+        System.out.println("************************s"+list.get(0));
+        return returnJson(objectMap, ErpInfo.OK.name, ErpInfo.OK.code);
     }
 
 }
