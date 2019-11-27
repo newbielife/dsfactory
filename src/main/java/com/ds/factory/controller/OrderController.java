7a4def7aa474b5a946798389af888ebd0dd26039
Lucien <562211065@qq.com>
2019-11-27 17:56:50 +0800

    增加订单详情查询

--- src/main/java/com/ds/factory/controller/OrderController.java
+++ src/main/java/com/ds/factory/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.ds.factory.controller;
 
 import com.ds.factory.constants.BusinessConstants;
+import com.ds.factory.service.Service.Order_DetailsService;
 import com.ds.factory.service.Service.Order_FormService;
 import com.ds.factory.utils.*;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +23,9 @@ import static com.ds.factory.utils.ResponseJsonUtil.returnJson;
 public class OrderController {
     @Resource
     Order_FormService order_formService;
+
+    @Resource
+    Order_DetailsService order_detailsService;
 
     @GetMapping(value = "/getAllOrder")
     public String getAllProductWarehouseList(@RequestParam(value = Constants.PAGE_SIZE, required = false) Integer pageSize,
@@ -50,5 +54,35 @@ public class OrderController {
         System.out.println("************************s"+list.get(0));
         return returnJson(objectMap, ErpInfo.OK.name, ErpInfo.OK.code);
     }
+
+    @GetMapping(value = "/getAllOrderDetail")
+    public String getAllOrderDetail(@RequestParam(value = Constants.PAGE_SIZE, required = false) Integer pageSize,
+                                             @RequestParam(value = Constants.CURRENT_PAGE, required = false) Integer currentPage,
+                                             @RequestParam(value = Constants.SEARCH, required = false) String search,
+                                             HttpServletRequest request)throws Exception {
+        Map<String, String> parameterMap = ParamUtils.requestToMap(request);
+        parameterMap.put(Constants.SEARCH, search);
+        PageQueryInfo queryInfo = new PageQueryInfo();
+        Map<String, Object> objectMap = new HashMap<String, Object>();
+        if (pageSize != null && pageSize <= 0) {
+            pageSize = 10;
+        }
+        String offset = ParamUtils.getPageOffset(currentPage, pageSize);
+        if (StringUtil.isNotEmpty(offset)) {
+            parameterMap.put(Constants.OFFSET, offset);
+        }
+        List<?> list = order_detailsService.orderByPopularity();
+        objectMap.put("page", queryInfo);
+        if (list == null) {
+            queryInfo.setRows(new ArrayList<Object>());
+            queryInfo.setTotal(BusinessConstants.DEFAULT_LIST_NULL_NUMBER);
+            return returnJson(objectMap, "查找不到数据", ErpInfo.OK.code);
+        }
+        queryInfo.setRows(list);
+        System.out.println("************************s"+list.get(0));
+        return returnJson(objectMap, ErpInfo.OK.name, ErpInfo.OK.code);
+    }
+
+
 
 }
