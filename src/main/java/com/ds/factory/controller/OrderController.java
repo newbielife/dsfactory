6370c8396f6758d3e08a5fe4a041d2d5adeb73d3
Lucien <562211065@qq.com>
2019-11-27 19:59:25 +0800

    退货bug

--- src/main/java/com/ds/factory/controller/OrderController.java
+++ src/main/java/com/ds/factory/controller/OrderController.java
@@ -1,8 +1,10 @@
 package com.ds.factory.controller;
 
 import com.ds.factory.constants.BusinessConstants;
+import com.ds.factory.service.Service.Export_RecordService;
 import com.ds.factory.service.Service.Order_DetailsService;
 import com.ds.factory.service.Service.Order_FormService;
+import com.ds.factory.service.Service.Refund_ApplicationService;
 import com.ds.factory.utils.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +28,12 @@ public class OrderController {
 
     @Resource
     Order_DetailsService order_detailsService;
+
+    @Resource
+    Export_RecordService export_recordService;
+
+    @Resource
+    Refund_ApplicationService refund_applicationService;
 
     @GetMapping(value = "/getAllOrder")
     public String getAllProductWarehouseList(@RequestParam(value = Constants.PAGE_SIZE, required = false) Integer pageSize,
@@ -72,6 +80,62 @@ public class OrderController {
             parameterMap.put(Constants.OFFSET, offset);
         }
         List<?> list = order_detailsService.selectByOrder_no("");
+        objectMap.put("page", queryInfo);
+        if (list == null) {
+            queryInfo.setRows(new ArrayList<Object>());
+            queryInfo.setTotal(BusinessConstants.DEFAULT_LIST_NULL_NUMBER);
+            return returnJson(objectMap, "查找不到数据", ErpInfo.OK.code);
+        }
+        queryInfo.setRows(list);
+        System.out.println("************************s"+list.get(0));
+        return returnJson(objectMap, ErpInfo.OK.name, ErpInfo.OK.code);
+    }
+
+    @GetMapping(value = "/getAllExportRecord")
+    public String getAllExportRecord(@RequestParam(value = Constants.PAGE_SIZE, required = false) Integer pageSize,
+                                    @RequestParam(value = Constants.CURRENT_PAGE, required = false) Integer currentPage,
+                                    @RequestParam(value = Constants.SEARCH, required = false) String search,
+                                    HttpServletRequest request)throws Exception {
+        Map<String, String> parameterMap = ParamUtils.requestToMap(request);
+        parameterMap.put(Constants.SEARCH, search);
+        PageQueryInfo queryInfo = new PageQueryInfo();
+        Map<String, Object> objectMap = new HashMap<String, Object>();
+        if (pageSize != null && pageSize <= 0) {
+            pageSize = 10;
+        }
+        String offset = ParamUtils.getPageOffset(currentPage, pageSize);
+        if (StringUtil.isNotEmpty(offset)) {
+            parameterMap.put(Constants.OFFSET, offset);
+        }
+        List<?> list = export_recordService.selectByOrder_no("");
+        objectMap.put("page", queryInfo);
+        if (list == null) {
+            queryInfo.setRows(new ArrayList<Object>());
+            queryInfo.setTotal(BusinessConstants.DEFAULT_LIST_NULL_NUMBER);
+            return returnJson(objectMap, "查找不到数据", ErpInfo.OK.code);
+        }
+        queryInfo.setRows(list);
+        System.out.println("************************s"+list.get(0));
+        return returnJson(objectMap, ErpInfo.OK.name, ErpInfo.OK.code);
+    }
+
+    @GetMapping(value = "/getAllRefund")
+    public String getAllRefund(@RequestParam(value = Constants.PAGE_SIZE, required = false) Integer pageSize,
+                                     @RequestParam(value = Constants.CURRENT_PAGE, required = false) Integer currentPage,
+                                     @RequestParam(value = Constants.SEARCH, required = false) String search,
+                                     HttpServletRequest request)throws Exception {
+        Map<String, String> parameterMap = ParamUtils.requestToMap(request);
+        parameterMap.put(Constants.SEARCH, search);
+        PageQueryInfo queryInfo = new PageQueryInfo();
+        Map<String, Object> objectMap = new HashMap<String, Object>();
+        if (pageSize != null && pageSize <= 0) {
+            pageSize = 10;
+        }
+        String offset = ParamUtils.getPageOffset(currentPage, pageSize);
+        if (StringUtil.isNotEmpty(offset)) {
+            parameterMap.put(Constants.OFFSET, offset);
+        }
+        List<?> list = refund_applicationService.selectByOrder_no("");
         objectMap.put("page", queryInfo);
         if (list == null) {
             queryInfo.setRows(new ArrayList<Object>());
