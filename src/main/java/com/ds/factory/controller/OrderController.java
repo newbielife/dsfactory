9ec7b2b89ef2637131524384ecf15a3aa7072979
Lucien <562211065@qq.com>
2019-11-28 11:52:41 +0800

    order细节查询

--- src/main/java/com/ds/factory/controller/OrderController.java
+++ src/main/java/com/ds/factory/controller/OrderController.java
@@ -1,11 +1,16 @@
 package com.ds.factory.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.ds.factory.constants.BusinessConstants;
+import com.ds.factory.datasource.entities.Order_Details;
 import com.ds.factory.service.Service.Export_RecordService;
 import com.ds.factory.service.Service.Order_DetailsService;
 import com.ds.factory.service.Service.Order_FormService;
 import com.ds.factory.service.Service.Refund_ApplicationService;
 import com.ds.factory.utils.*;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,10 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static com.ds.factory.utils.ResponseJsonUtil.returnJson;
 
@@ -68,18 +70,54 @@ public class OrderController {
                                              @RequestParam(value = Constants.CURRENT_PAGE, required = false) Integer currentPage,
                                              @RequestParam(value = Constants.SEARCH, required = false) String search,
                                              HttpServletRequest request)throws Exception {
-        Map<String, String> parameterMap = ParamUtils.requestToMap(request);
-        parameterMap.put(Constants.SEARCH, search);
+//        Map<String, String> parameterMap = ParamUtils.requestToMap(request);
+//        parameterMap.put(Constants.SEARCH, search);
+//        String no=parameterMap.get("no");
+//        String client_no=parameterMap.get("client_no");
+//        String product_no=parameterMap.get("product_no");
+//        String check=parameterMap.get("check");
+//        PageQueryInfo queryInfo = new PageQueryInfo();
+//        Map<String, Object> objectMap = new HashMap<String, Object>();
+//        if (pageSize != null && pageSize <= 0) {
+//            pageSize = 10;
+//        }
+//        String offset = ParamUtils.getPageOffset(currentPage, pageSize);
+//        if (StringUtil.isNotEmpty(offset)) {
+//            parameterMap.put(Constants.OFFSET, offset);
+//        }
+//        List<?> list = order_detailsService.selectByConstraint(no,client_no,product_no,check);
+//        objectMap.put("page", queryInfo);
+//        if (list == null) {
+//            queryInfo.setRows(new ArrayList<Object>());
+//            queryInfo.setTotal(BusinessConstants.DEFAULT_LIST_NULL_NUMBER);
+//            return returnJson(objectMap, "查找不到数据", ErpInfo.OK.code);
+//        }
+//        queryInfo.setRows(list);
+//        System.out.println("************************s"+list.get(0));
+//        return returnJson(objectMap, ErpInfo.OK.name, ErpInfo.OK.code);
+        Map<String, String> parameterMap = new HashMap<String, String>();
+        //查询参数
+        JSONObject obj= JSON.parseObject(search);
+        Set<String> key= obj.keySet();
+        for(String keyEach: key){
+            parameterMap.put(keyEach,obj.getString(keyEach));
+        }
+        String no=parameterMap.get("no");
+        String client_no=parameterMap.get("client_no");
+        String product_no=parameterMap.get("product_no");
+        String check=parameterMap.get("check");
         PageQueryInfo queryInfo = new PageQueryInfo();
         Map<String, Object> objectMap = new HashMap<String, Object>();
-        if (pageSize != null && pageSize <= 0) {
-            pageSize = 10;
+        if (pageSize == null || pageSize <= 0) {
+            pageSize = BusinessConstants.DEFAULT_PAGINATION_PAGE_SIZE;
         }
-        String offset = ParamUtils.getPageOffset(currentPage, pageSize);
-        if (StringUtil.isNotEmpty(offset)) {
-            parameterMap.put(Constants.OFFSET, offset);
+        if (currentPage == null || currentPage <= 0) {
+            currentPage = BusinessConstants.DEFAULT_PAGINATION_PAGE_NUMBER;
         }
-        List<?> list = order_detailsService.selectByOrder_no("");
+        PageHelper.startPage(currentPage,pageSize,true);
+        List<Order_Details> list = order_detailsService.selectByConstraint(no,client_no,product_no,check);
+        //获取分页查询后的数据
+        PageInfo<Order_Details> pageInfo = new PageInfo<>(list);
         objectMap.put("page", queryInfo);
         if (list == null) {
             queryInfo.setRows(new ArrayList<Object>());
@@ -87,7 +125,7 @@ public class OrderController {
             return returnJson(objectMap, "查找不到数据", ErpInfo.OK.code);
         }
         queryInfo.setRows(list);
-        System.out.println("************************s"+list.get(0));
+        queryInfo.setTotal(pageInfo.getTotal());
         return returnJson(objectMap, ErpInfo.OK.name, ErpInfo.OK.code);
     }
 
